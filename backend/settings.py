42e7ad89cd3ddaa63c49eccf2f825b7317dae513
regan-mu <regansomi@gmail.com>
2024-05-28 22:24:19 +0300

    Add client side CORS origin

--- backend/settings.py
+++ backend/settings.py
@@ -144,6 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://kithima-development-client.vercel.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
